ddf168ce6231fbb7abd5a7bb4e26a0bb0d289a84
Joe Baker <joeblakeb@gmail.com>
2022-05-11 18:36:33 +0100

    logout and api.getUser

--- scripts/api.js
+++ scripts/api.js
@@ -1,27 +1,41 @@
 "use strict";
 
 const api = {
-    domain: "https://api.cool-computer-club.com/",
-    // domain: "http://localhost:8080/",
-    httpGet: function (endpoint, onFinish) {
+    domain: "http://localhost:8080/",
+    username: "",
+    accessLevel: "",
+    request: function (type, endpoint, onFinish, json) {
         let req = new XMLHttpRequest();
-        req.onreadystatechange = function () {
-            if (req.readyState == 4) {
-                onFinish(req);
+        if (onFinish != null) {
+            req.onreadystatechange = function () {
+                if (req.readyState == 4) {
+                    onFinish(req);
+                }
             }
         }
-        req.open("GET", this.domain + endpoint);
-        req.send();
+        req.open(type, this.domain + endpoint);
+        if (type == "POST" || type == "PUT") {
+            req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+            req.send(JSON.stringify(json));
+        }
+        else {
+            req.send();
+        }
     },
-    httpPost: function (endpoint, json, onFinish) {
-        let req = new XMLHttpRequest();
-        req.onreadystatechange = function () {
-            if (req.readyState == 4) {
-                onFinish(req);
+    // Returns an object with username and accessLevel
+    // both will be "null" if not logged in
+    getUser: function () {
+        let user = {};
+        let cookies = document.cookie.split("; ");
+        for (let cookie of cookies) {
+            cookie = cookie.split("=");
+            if (cookie[0] == "username") {
+                user.username = cookie[1];
+            }
+            if (cookie[0] == "accessLevel") {
+                user.accessLevel = cookie[1];
             }
         }
-        req.open("POST", this.domain + endpoint);
-        req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-        req.send(JSON.stringify(json));
+        return user;
     }
 };
--- scripts/login.js
+++ scripts/login.js
@@ -12,14 +12,22 @@ loginButton.addEventListener("click", (e) => {
 });
 
 function login (username, password) {
-    api.httpPost("login",
-    {username:username, password:password},
+    api.request("POST", "login",
     function (req) {
         if (req.status == 200) {
+            let data = JSON.parse(req.responseText);
+            document.cookie = "username=" + data.username + ";sameSite=Lax;max-age=31536000";
+            document.cookie = "accessLevel=" + data.accessLevel + ";sameSite=Lax;max-age=31536000";
             window.location.replace("index.html");
         }
         else {
             alert("Username or password is invalid");
         }
-    })
+    }, {username:username, password:password})
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    api.request('GET', 'logout');
+    document.cookie = "username=null;sameSite=Lax;max-age=31536000";
+    document.cookie = "accessLevel=null;sameSite=Lax;max-age=31536000";
+});
