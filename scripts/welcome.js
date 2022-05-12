f7836fb52c7da5c79abff01425690d84422f0c5d
Neopickle03 <hammond-montana@hotmail.com>
2022-05-12 09:02:15 +0100

    Fix login temp

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
     // get user object
     user = api.getUser();
     // if not logged in, go to log in page
-    if (user.username == "null") {
-        window.location.replace("login.html");
-    }
+    // if (user.username == "null") {
+    //     window.location.replace("login.html");
+    // }
     // add message
     document.getElementById("welcomeMessage").innerHTML = "Welcome " + user.username + " [" + user.accessLevel + "]";
 });
