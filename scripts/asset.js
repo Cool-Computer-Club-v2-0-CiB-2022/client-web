de29b582777f58ed9dcc90060f3d2c089850a156
Joe Baker <joeblakeb@gmail.com>
2022-05-12 05:26:55 +0100

    asset list & fix DWK_trans & add favicon

--- scripts/asset.js
+++ scripts/asset.js
@@ -1,1 +1,43 @@
-document.getElementById("btnSearchAssets").addEventListener('click', searchAsset);
+// document.getElementById("btnSearchAssets").addEventListener('click', searchAsset);
+
+// soo that the names of fields arent whats in the db
+let fieldNames = {
+    assetInventoryNumber: "AssetID",
+    assetName: "Asset Name",
+    type: "Type",
+    typePresence: "Type Presence",
+    location: "Location",
+    locationCode: "Location Code",
+    locationType: "Location Type",
+    resolverQueue: "Resolver Queue",
+    status: "Status",
+    subStatus: "Sub-Status",
+    assignedTo: "Assigned To",
+    billedTo: "Billed To",
+    dateCreated: "Date Created",
+    dateActive: "Date Active",
+    dateInstalled: "Date Installed",
+    dateDecomm: "Date Decommissioned",
+    maintenanceWindow: "Maintenance Window"
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+    api.request('GET', 'report.json',
+    function (resp) {
+        // load object
+        let assets = JSON.parse(resp.responseText);
+        let newHtml = "";
+        // go through each asset and generate its html
+        for (let asset of Object.values(assets.data)) {
+            let assetHtml = "<li>";
+            // go through each field of the asset
+            for (let field of Object.keys(asset)) {
+                assetHtml += "<b>" + fieldNames[field] + ":</b> " + asset[field] + " | ";
+                console.log(field);
+            }
+            newHtml += assetHtml.slice(0, -3) + "</li>";
+        }
+        // add to html
+        document.getElementById("assetTableInsert").innerHTML = newHtml;
+    });
+});
